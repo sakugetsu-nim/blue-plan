1848f37fb485132a5c73ad97e7ee065ee17f7278
sakugetsu <toyotama.yahiro@live.jp>
2024-05-11 22:02:32 +0900

    calendar画面に各所へのリンクを追加。

--- app/models/Plan.rb
+++ app/models/Plan.rb
@@ -3,7 +3,7 @@ class Plan < ApplicationRecord
 
 
   #追加
-  validates :title, presence: true, length: { maximum: 50 } #最大文字数５０文字
+  validates :title, presence: true, length: { maximum: 20 } #最大文字数５０文字
   validates :content, presence: true
   validates :start_time, presence: true
   validates :user_id, presence: true
