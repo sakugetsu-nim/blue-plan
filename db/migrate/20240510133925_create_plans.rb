78e4606c33d8a55f7dc998bb0fe501ce66189d05
HIRO <tekuport@gmail.com>
2024-05-13 18:01:40 +0900

    Revert "不要なデータ削除マージ"
    This reverts commit 3a751761849c76cee27411d3573d1433847afe67.

---
+++ db/migrate/20240510133925_create_plans.rb
@@ -0,0 +1,11 @@
+class CreatePlans < ActiveRecord::Migration[7.1]
+  def change
+    create_table :plans do |t|
+      t.string :title
+      t.text :content
+      t.datetime :start_time
+
+      t.timestamps
+    end
+  end
+end
