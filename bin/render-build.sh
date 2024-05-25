0d63570be58795c89f6987e0c73963449a24f964
HIRO <tekuport@gmail.com>
2024-05-26 01:28:15 +0900

    再

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,1 +1,1 @@
-bundle install && yarn install && bundle exec rake assets:precompile && bundle exec rake assets:clean && bundle exec rails webpacker:compile && bundle exec rake db:migrate
+bundle install && yarn install && bundle exec rake assets:precompile && bundle exec rake assets:clean && bundle exec rails webpacker:compile && bundle exec rake db:migrate && bundle exec rails db:seed
