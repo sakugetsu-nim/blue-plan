8666d1b1cdcee9f9eb5c4d90cf7e201ffcddd0ac
sakugetsu <toyotama.yahiro@live.jp>
2024-05-25 11:10:31 +0900

    ユーザー名の登録とcalendarでの表示に関するviewの編集を行う

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
-    protected
-  
-    def after_sign_out_path_for(resource_or_scope)
-      new_user_session_path
-    end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
-  
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+
+end
+
