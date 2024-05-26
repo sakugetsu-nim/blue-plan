aced2aab725069420a5cc977a461a8aa6c9465d7
HIRO <tekuport@gmail.com>
2024-05-26 09:58:20 +0900

    再

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,13 +2,13 @@
 # exit on error
 set -o errexit
 
-# bundle install
-# yarn install
-# bundle exec rails assets:precompile
-# bundle exec rails assets:clean
-# bundle exec rails webpacker:compile
-# bundle exec rails db:migrate
-# bundle exec rails db:seed
+bundle install
+yarn install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake webpacker:compile
+bundle exec rake db:migrate
+bundle exec rake db:seed
 
 
-bundle install && yarn install && bundle exec rake assets:precompile && bundle exec rake assets:clean && bundle exec rails webpacker:compile && bundle exec rake db:migrate && bundle exec rails db:seed
+# bundle install && yarn install && bundle exec rake assets:precompile && bundle exec rake assets:clean && bundle exec rails webpacker:compile && bundle exec rake db:migrate && bundle exec rails db:seed
